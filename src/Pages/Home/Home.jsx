e34557eaa3bc3a22dfda129eafd0d4adc28290c5
unknown <hari11122003@gmail.com>
2023-07-07 19:55:40 +0530

    first commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,11 +4,19 @@ import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
 import HomeMainbar from '../../components/HomeMainbar/HomeMainbar'
 import '../../App.css'
 
+
 const Home = () => {
+  const imageStyle = {
+    backgroundImage: 'url("../../assets/home-background.png")',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  };
+
   return (
     <div className='home-container-1'>
       <LeftSidebar />
-      <div className='home-container-2'>
+      <div className='home-container-2' style={imageStyle}>
         <HomeMainbar />
         <RightSidebar />
       </div>
