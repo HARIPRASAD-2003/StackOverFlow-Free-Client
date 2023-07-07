d8846ec6ff2fe61e41e4d735d1a75ac5a14b2ed9
unknown <hari11122003@gmail.com>
2023-07-07 20:41:21 +0530

    first commit

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,48 +1,55 @@
 import React from 'react';
-import './HomePage.css';
+import './HomePage.css'; // Import the CSS file for styling
 
 const HomePage = () => {
   return (
     <div className="homepage">
-      <header className="header">
-        <div className="logo">Stack Overflow Logo</div>
-        <input type="text" className="search-input" placeholder="Search" />
-        <nav className="navigation">
+      <div className="image-overlay">
+        <img src="./home-background.png" alt="Background" />
+        <div className="overlay-content">
+          <h1>Welcome to Stack Overflow</h1>
+          <p>Ask questions, get answers, and share knowledge</p>
+        </div>
+      </div>
+
+      <section className="hero-section overlay-section">
+        <div className="hero-image-overlay">
+          <img src="./home-background.png" alt="Hero" />
+        </div>
+        <div className="overlay-content">
+          <h2>Hero Section</h2>
+          <p>Some text for the hero section</p>
+        </div>
+      </section>
+
+      <section className="featured-questions overlay-section">
+        <div className="featured-image-overlay">
+          <img src="./home-background.png" alt="Featured" />
+        </div>
+        <div className="overlay-content">
+          <h2>Featured Questions</h2>
           <ul>
-            <li>Questions</li>
-            <li>Tags</li>
-            <li>Users</li>
-            <li>Jobs</li>
-            <li>Documentation</li>
+            <li>
+              <h3>Question Title 1</h3>
+              <p>Question summary</p>
+              <p>Views: 100 | Votes: 10</p>
+            </li>
+            <li>
+              <h3>Question Title 2</h3>
+              <p>Question summary</p>
+              <p>Views: 200 | Votes: 15</p>
+            </li>
           </ul>
-        </nav>
-        <button className="login-button">Login/Signup</button>
-      </header>
-
-      <section className="hero-section">
-        <h1>Ask questions, get answers, and share knowledge</h1>
-        <button className="ask-button">Ask your first question</button>
+        </div>
       </section>
 
-      <section className="featured-questions">
-        <h2>Featured Questions</h2>
-        <ul>
-          <li>
-            <h3>Question Title 1</h3>
-            <p>Question summary</p>
-            <p>Views: 100 | Votes: 10</p>
-          </li>
-          <li>
-            <h3>Question Title 2</h3>
-            <p>Question summary</p>
-            <p>Views: 200 | Votes: 15</p>
-          </li>
-        </ul>
-        <button className="view-more-button">View More</button>
-      </section>
-
-      <footer className="footer">
-        <p>&copy; 2023 Stack Overflow. All rights reserved.</p>
+      <footer className="footer overlay-section">
+        <div className="footer-image-overlay">
+          <img src="./home-background.png" alt="Footer" />
+        </div>
+        <div className="overlay-content">
+          <p>&copy; 2023 Stack Overflow. All rights reserved.</p>
+        </div>
       </footer>
     </div>
   );
