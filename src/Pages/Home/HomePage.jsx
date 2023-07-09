2c9e3b591abe8c51408c1d44cbf7740b7842a6d0
unknown <hari11122003@gmail.com>
2023-07-09 14:08:17 +0530

    first commit

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -30,32 +30,36 @@ const HomePage = () => {
           <p>Ask questions, get answers, and share knowledge</p>
         </div>
       </div>
-      <section className='overlay-section'>
+      <section className='overlay-section' style={{maxWidth:'100%', width: '100%' }}>
 
-      <div className="image-overlay" style={{ width: '100%' }}>
-        <img src={home_background_2} alt="Background" />
+      <div className="image-overlay overlay-section" >
+        <img src={home_background_2} alt="Background" width='100%' />
         <div className="overlay-content" style={{ borderRadius: '10px', padding: '10px', paddingBottom: '30px' }}>
-            <h2>Discover Our Platform</h2>
-            <p>
-            TECH MARVEL is a question and answer website for professional and enthusiast programmers. It is a
-            community-driven platform where developers can ask questions, provide answers, and share knowledge.
-            </p>
-            <button className="ask-button" onClick={checkAuth}>Ask your first question</button>
-            <div className='largeScreens'>
-            <h3>Join our community to:</h3>
-            <ul>
-            <li><p>Connect with other developers</p></li>
-            <li><p>Ask and answer questions</p></li>
-            <li><p>Share your expertise</p></li>
-            <li><p>Stay updated with the latest technologies</p></li>
-            </ul>
-            </div>
+        <h2>Discover Our Platform</h2>
+        <p>
+          Welcome to TECH MARVEL, the ultimate destination for programmers and tech enthusiasts. With our platform, you'll embark on a transformative coding journey. Here's what makes us special:
+        </p>
+        <button className="ask-button" onClick={checkAuth}>Ask your first question</button>
+        <div className='largeScreens'>
+          <h3>Join our community to:</h3>
+          <ul>
+            <li><p>Unlock hidden programming techniques</p></li>
+            <li><p>Collaborate with industry professionals</p></li>
+            <li><p>Discover cutting-edge technologies</p></li>
+            <li><p>Immerse yourself in coding challenges</p></li>
+            <li><p>Access exclusive coding resources</p></li>
+          </ul>
+        </div>
+        <p>
+          At TECH MARVEL, we believe in fostering a vibrant and supportive community. Whether you're a beginner or an experienced coder, you'll find a wealth of knowledge and expertise to fuel your programming journey. Our platform offers a range of features to help you grow, including interactive discussions, expert answers, and access to the latest industry trends. Join us today and unlock your full coding potential!
+        </p>
+
         </div>
       </div>
       </section>
-      <section className='overlay-section'>
+      <section className='overlay-section' style={{maxWidth:'100%', width: '100%' }}>
 
-      <div className="image-overlay" style={{ width: '100%' }}>
+      <div className="image-overlay  overlay-section" style={{ width: '100%' }}>
         <img src={home_background_3} alt="Background"/>
         <div className="overlay-content">
           <h2>Featured Questions</h2>
@@ -76,13 +80,30 @@ const HomePage = () => {
               </p>
               <p>Views: 200 | Votes: 15</p>
             </li>
+            <li className="largeScreens">
+              <h3>How to optimize website performance?</h3>
+              <p>
+                My website is running slow. What are some strategies and techniques to optimize website performance and improve
+                loading speed?
+              </p>
+              <p>Views: 150 | Votes: 10</p>
+            </li>
+            <li className="largeScreens">
+              <h3>How to secure a web application?</h3>
+              <p>
+                I want to ensure the security of my web application. What are some best practices and techniques to protect
+                against common vulnerabilities and secure the application?
+              </p>
+              <p>Views: 180 | Votes: 12</p>
+            </li>
+
           </ul>
           <button className="view-more-button" onClick={() => navigate('/Questions')}>View More</button>
         </div>
       </div>
       </section>
-      <section className='overlay-section'>
-        <div className="image-overlay " style={{ width: '100%' }}>
+      <section className='overlay-section ' style={{maxWidth:'100%', width: '100%' }}>
+        <div className="image-overlay overlay-section" style={{ width: '100%' }}>
             <img src={home_background_4} alt="Background" />
             <div className="overlay-content">
             <h2>About TECH MARVEL</h2>
@@ -91,14 +112,19 @@ const HomePage = () => {
             community-driven platform where developers can ask questions, provide answers, and share knowledge.
             </p>
             <div className='largeScreens'>
-            <h3>Why Choose TECH MARVEL:</h3>
-            <ul>
-            <li>Wide range of technology topics covered</li>
-            <li>Active and supportive community</li>
-            <li>Quality answers from experts</li>
-            <li>User-friendly interface</li>
-            <li>Regular updates and improvements</li>
-            </ul>
+              <h3>Our Vision:</h3>
+                <p>
+                  We envision a world where every programmer has access to high-quality resources, mentorship, and a supportive
+                  community. Our goal is to foster innovation, creativity, and excellence in the software development industry.
+                </p>
+              <h3>Why Choose TECH MARVEL:</h3>
+                <ul>
+                  <li>Wide range of technology topics covered</li>
+                  <li>Active and supportive community</li>
+                  <li>Quality answers from experts</li>
+                  <li>User-friendly interface</li>
+                  <li>Regular updates and improvements</li>
+                </ul>
             </div>
             <p>
             Join TECH MARVEL today and enhance your programming knowledge while connecting with a vibrant community of
